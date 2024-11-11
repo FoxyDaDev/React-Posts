2ba9883e01309daaa4cef142c4f12f67c7398c63
FoxyDaDev <150698022+FoxyDaDev@users.noreply.github.com>
2024-11-11 17:48:04 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "greenish": "#117768",
+        "greenish-hold": "#0d594e",
+        "backgroundish": "#F5F5F4",
+        "holdish": "#F8F7F1",
+        "bleh": "#BBCAB0",
+      }
+    },
+  },
+  plugins: [],
+}
